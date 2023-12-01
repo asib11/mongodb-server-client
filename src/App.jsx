5a44e8d97ddc3ed1280f9852994776257386066d
asib ahmed <ahmedasib1@gmail.com>
2023-12-01 17:18:02 +0600

    first CRUD operation

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,19 @@ function App() {
     const name = form.name.value;
     const email = form.email.value;
     const user = {name, email};
-    console.log(user)
+    console.log(user);
+
+    fetch('http://localhost:5000/users',{
+      method: 'POST',
+      headers:{
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data =>{
+      console.log(data)
+    })
   }
 
   return (
