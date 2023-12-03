404a899d3ef8082e4854159930123831788b155b
asib ahmed <ahmedasib1@gmail.com>
2023-12-04 00:09:20 +0600

    update data send to server

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -8,7 +8,19 @@ const Update = () => {
         const form = event.target;
         const name = form.name.value;
         const email = form.email.value;
-        
+        const updatedUser = {name, email};
+
+        fetch(`http://localhost:5000/users/${loadedUser._id}`,{
+            method: 'PUT',
+            headers:{
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(updatedUser)
+        })
+        .then(res => res.json())
+        .then(data =>{
+            console.log(data);
+        })
     }
     return (
         <div>
