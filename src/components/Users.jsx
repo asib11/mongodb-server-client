b4da6a21666930914137c74610838127c00c547d
asib ahmed <ahmedasib1@gmail.com>
2023-12-03 17:50:22 +0600

    delete data from client side

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,19 +1,27 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 
 const Users = () => {
-    const users = useLoaderData();
-    const handleDelete = _id =>{
+    const loadUsers = useLoaderData();
+    const [users, setUsers] = useState(loadUsers);
+
+    const handleDelete = _id => {
         console.log('delte', _id);
-        fetch(`http://localhost:5000/users/${_id}`,{
+        fetch(`http://localhost:5000/users/${_id}`, {
             method: 'DELETE',
             // headers: {
             //     'content-type': 'application/json'
             // },
             // body: JSON.stringify(_id)
         })
-        .then(res => res.json())
-        .then(data => console.log(data))
+            .then(res => res.json())
+            .then(data => {
+                if (data.deletedCount > 0) {
+                    const remaining = users.filter(user => user._id !== _id);
+                    setUsers(remaining);
+                }
+            })
     }
 
     return (
